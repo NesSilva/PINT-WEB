684ac71cc5ff24bb21e1af0f65eb6e06581dd49a
Jose Beselga <beselgamain@gmail.com>
2025-05-14 19:39:19 +0100

    [FIX] Script de Executar os Serviços

--- exec.bat
+++ exec.bat
@@ -1,16 +1,14 @@
 @echo off
-REM Sai se algum comando falhar
-
 REM Inicia o backend na porta 3001
 echo 🚀 Backend: porta 3001
 cd backend
 npm install
-set PORT=3001
-start "Backend" cmd /k "npm run dev"
+start "Backend" cmd /k "set PORT=3001 && npm run dev"
 
 REM Inicia o frontend na porta 3000
 echo 🎨 Frontend: porta 3000
 cd ..\frontend
 npm install
-set PORT=3000
-start "Frontend" cmd /k "npm start"
+start "Frontend" cmd /k "set PORT=3000 && npm start"
+
+pause  "Pressione qualquer tecla para sair..."
