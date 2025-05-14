e55c3eb6f096cd261d3de02ca9c2e46bd535aad2
Jose Beselga <beselgamain@gmail.com>
2025-05-14 19:02:51 +0100

    [ADD] PostgresSQL Server Configured

--- backend/models/basededados.js
+++ backend/models/basededados.js
@@ -1,13 +1,41 @@
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize(
-    'bd_pint',
-    'postgres',
-    'ines27ma',
-    {
-        host: 'localhost',
-        port: 5432,
-        dialect: 'postgres'
-    }
+// config.js
+const fs        = require('fs');
+const path      = require('path');
+const Sequelize = require('sequelize');
+
+const DB_NAME = 'defaultdb';
+const DB_USER = 'avnadmin';
+const DB_PASS = 'AVNS_0dRXW1jAWGD63pNyiKq';
+const DB_HOST = 'pg-pint-jmvbeselga-d39c.e.aivencloud.com';
+const DB_PORT = 11754;
+
+const CA_CERT_PATH = path.join(__dirname, 'aiven-ca.crt');
+
+
+const caCert = fs.readFileSync(CA_CERT_PATH).toString();
+
+const sequelize = new Sequelize(
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+    pool: {
+      max: 20,       
+      min: 0,
+      idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,           
+        rejectUnauthorized: true,
+        ca: caCert               
+      }
+    },
+   
+  }
 );
 
 module.exports = sequelize;
